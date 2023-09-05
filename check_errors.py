20116325e3a4bc5f1cc36dc7699b5f29408135a9
Julian Falco <julian.falco@utexas.edu>
2023-09-05 12:36:02 -0600

    Add program to check how many errors

---
+++ check_errors.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+os.chdir(os.path.join("..", "testing_logs", "oblique"))
+
+errors = 0
+total = 0
+for fname in os.listdir("."):
+    with open(fname, "r") as f:
+        total += 1
+        log_data = json.load(f)
+        if log_data["exception"]:
+            errors += 1
+
+print(f"Errors: {errors}/{total}")
+
