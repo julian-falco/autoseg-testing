a7a61f78eb74f94f7ba192143a91a7550633c7a5
Julian Falco <julian.falco@utexas.edu>
2023-09-05 13:21:27 -0600

    Remove message tracking for exceptions

--- check_errors.py
+++ check_errors.py
@@ -5,18 +5,13 @@ os.chdir(os.path.join("..", "testing_log", "oblique"))
 
 errors = 0
 total = 0
-messages = set()
 for fname in os.listdir("."):
     with open(fname, "r") as f:
         total += 1
         log_data = json.load(f)
         if log_data["exception"]:
             errors += 1
-            messages.add(log_data["exception"])
+        else:
+            print(fname)
 
 print(f"Errors: {errors}/{total}")
-
-for message in messages:
-    print(message)
-    print()
-
